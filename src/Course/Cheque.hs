b7b835a037af4fc57500bcd6c2324329dc6d7cf2
Sergey Kurgak <kurgak.ss@gmail.com>
2021-07-02 14:56:57 +1100

    Cheque solved

--- src/Course/Cheque.hs
+++ src/Course/Cheque.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 
 {-
 
@@ -30,11 +31,9 @@ import Course.Monad
 -- >>> :set -XOverloadedStrings
 
 -- The representation of the grouping of each exponent of one thousand. ["thousand", "million", ...]
-illion ::
-  List Chars
+illion :: List Chars
 illion =
-  let preillion ::
-        List (Chars -> Chars)
+  let preillion :: List (Chars -> Chars)
       preillion =
         listh [
           const ""
@@ -48,8 +47,7 @@ illion =
         , const "octo"
         , \q -> if "n" `isPrefixOf` q then "novem" else "noven"
         ]
-      postillion ::
-        List Chars
+      postillion :: List Chars
       postillion =
         listh [
           "vigintillion"
@@ -189,63 +187,37 @@ data Digit =
   | Nine
   deriving (Eq, Ord)
 
-showDigit ::
-  Digit
-  -> Chars
-showDigit Zero =
-  "zero"
-showDigit One =
-  "one"
-showDigit Two =
-  "two"
-showDigit Three =
-  "three"
-showDigit Four =
-  "four"
-showDigit Five =
-  "five"
-showDigit Six =
-  "six"
-showDigit Seven =
-  "seven"
-showDigit Eight =
-  "eight"
-showDigit Nine =
-  "nine"
+showDigit :: Digit -> Chars
+showDigit Zero = "zero"
+showDigit One = "one"
+showDigit Two = "two"
+showDigit Three = "three"
+showDigit Four = "four"
+showDigit Five = "five"
+showDigit Six = "six"
+showDigit Seven = "seven"
+showDigit Eight = "eight"
+showDigit Nine = "nine"
 
 -- A data type representing one, two or three digits, which may be useful for grouping.
-data Digit3 =
-  D1 Digit
-  | D2 Digit Digit
-  | D3 Digit Digit Digit
-  deriving Eq
+data Digit3 = D1 Digit
+            | D2 Digit Digit
+            | D3 Digit Digit Digit
+            deriving Eq
 
 -- Possibly convert a character to a digit.
-fromChar ::
-  Char
-  -> Optional Digit
-fromChar '0' =
-  Full Zero
-fromChar '1' =
-  Full One
-fromChar '2' =
-  Full Two
-fromChar '3' =
-  Full Three
-fromChar '4' =
-  Full Four
-fromChar '5' =
-  Full Five
-fromChar '6' =
-  Full Six
-fromChar '7' =
-  Full Seven
-fromChar '8' =
-  Full Eight
-fromChar '9' =
-  Full Nine
-fromChar _ =
-  Empty
+fromChar :: Char -> Optional Digit
+fromChar '0' = Full Zero
+fromChar '1' = Full One
+fromChar '2' = Full Two
+fromChar '3' = Full Three
+fromChar '4' = Full Four
+fromChar '5' = Full Five
+fromChar '6' = Full Six
+fromChar '7' = Full Seven
+fromChar '8' = Full Eight
+fromChar '9' = Full Nine
+fromChar _   = Empty
 
 -- | Take a numeric value and produce its English output.
 --
@@ -319,9 +291,91 @@ fromChar _ =
 -- "twelve thousand three hundred and forty-five dollars and sixty-seven cents"
 --
 -- >>> dollars "456789123456789012345678901234567890123456789012345678901234567890.12"
--- "four hundred and fifty-six vigintillion seven hundred and eighty-nine novemdecillion one hundred and twenty-three octodecillion four hundred and fifty-six septendecillion seven hundred and eighty-nine sexdecillion twelve quindecillion three hundred and forty-five quattuordecillion six hundred and seventy-eight tredecillion nine hundred and one duodecillion two hundred and thirty-four undecillion five hundred and sixty-seven decillion eight hundred and ninety nonillion one hundred and twenty-three octillion four hundred and fifty-six septillion seven hundred and eighty-nine sextillion twelve quintillion three hundred and forty-five quadrillion six hundred and seventy-eight trillion nine hundred and one billion two hundred and thirty-four million five hundred and sixty-seven thousand eight hundred and ninety dollars and twelve cents"
-dollars ::
-  Chars
-  -> Chars
-dollars =
-  error "todo: Course.Cheque#dollars"
+-- "four hundred and fifty-six vigintillion seven hundred and eighty-nine novemdecillion
+--  one hundred and twenty-three octodecillion four hundred and fifty-six septendecillion
+--  seven hundred and eighty-nine sexdecillion twelve quindecillion three hundred and forty-five quattuordecillion
+--  six hundred and seventy-eight tredecillion nine hundred and one duodecillion two hundred and thirty-four undecillion
+--  five hundred and sixty-seven decillion eight hundred and ninety nonillion one hundred and twenty-three octillion
+--  four hundred and fifty-six septillion seven hundred and eighty-nine sextillion twelve quintillion
+--  three hundred and forty-five quadrillion six hundred and seventy-eight trillion nine hundred and one billion
+--  two hundred and thirty-four million five hundred and sixty-seven thousand eight hundred and ninety dollars and twelve cents"
+
+showDigit3 :: Digit3 -> Chars
+showDigit3 = \case
+    (D1 d1)            -> showDigit d1
+    (D2 Zero d1)       -> showDigit d1
+    (D2 One Zero)      -> "ten"
+    (D2 One One)       -> "eleven"
+    (D2 One Two)       -> "twelve"
+    (D2 One Three)     -> "thirteen"
+    (D2 One d1)        -> showDigit d1 ++ "teen"
+    (D2 Two d1 )       -> "twenty" ++ showUnits d1
+    (D2 Three d1 )     -> "thirty" ++ showUnits d1
+    (D2 Four d1 )      -> "forty" ++ showUnits d1
+    (D2 Five d1 )      -> "fifty" ++ showUnits d1
+    (D2 Six d1 )       -> "sixty" ++ showUnits d1
+    (D2 Seven d1 )     -> "seventy" ++ showUnits d1
+    (D2 Eight d1 )     -> "eighty" ++ showUnits d1
+    (D2 Nine d1 )      -> "ninety" ++ showUnits d1
+    (D3 Zero d2 d1)    -> showDigit3 (D2 d2 d1)
+    (D3 d3 Zero Zero)  -> showHundreds d3
+    (D3 d3 d2 d1)      -> showHundreds d3 ++ " and " ++ showDigit3 (D2 d2 d1)
+  where
+    showHundreds Zero = ""
+    showHundreds d    = showDigit d ++ " hundred"
+    showUnits Zero    = ""
+    showUnits d       = "-" ++ showDigit d
+
+dollarBuildStr :: List (Chars, Digit3) -> Chars
+dollarBuildStr = foldRight func "zero dollars"
+  where
+    func ("", d@(D1 One)) lst        = "one dollar"
+    func ("", d@(D2 Zero One)) lst   = "one dollar"
+    func ("", d@(D3 _ Zero One)) lst = showDigit3 d ++ " dollar"
+    func ("", d) lst                 = showDigit3 d ++ " dollars"
+    func (_, D1 Zero) lst            = lst
+    func (_, D2 Zero Zero) lst       = lst
+    func (_, D3 Zero Zero Zero) lst  = lst
+    func (str, d) lst                = showDigit3 d ++ " " ++ str ++ " " ++ lst
+
+centBuildStr :: Digit3 -> Chars
+centBuildStr = \case
+  d@(D2 Zero One) -> showDigit3 d ++ " cent"
+  d               -> showDigit3 d ++ " cents"
+
+centsToD3 :: List Digit -> Digit3
+centsToD3 = \case
+  Nil             -> D2 Zero Zero
+  (d2 :. Nil)     -> D2 d2 Zero
+  (d2 :. d1 :. _) -> D2 d2 d1
+
+dollarsToD3Lst :: List Digit -> List Digit3
+dollarsToD3Lst = foldRight func Nil
+  where
+    func d = \case
+      (D1 d1) :. lst    -> (D2 d d1) :. lst
+      (D2 d2 d1) :. lst -> (D3 d d2 d1) :. lst
+      lst               -> (D1 d) :. lst
+
+withGroupName :: List Digit3 -> List (Chars, Digit3)
+withGroupName = reverse . zip illion . reverse
+
+parseChars :: Chars -> (List Digit, List Digit)
+parseChars lst = (reverse ds,reverse cs)
+  where
+    ((ds, cs), comma) = foldLeft f ((Nil,Nil),False) lst
+    f rest@((dollars,cents), comma) c
+      | c == '.'   = ((dollars,cents), True)
+      | otherwise  = case (fromChar c, comma) of
+                      (Full d, True)  -> ((dollars, d :. cents), True)
+                      (Full d, False) -> ((d :. dollars, cents), False)
+                      _               -> rest
+
+dollars :: Chars -> Chars
+dollars lst = dsString ++ " and " ++ csString
+  where
+    (ds, cs)  = parseChars lst
+    csD3      = centsToD3 cs
+    dsD3Named = withGroupName $ dollarsToD3Lst ds
+    dsString  = dollarBuildStr dsD3Named
+    csString  = centBuildStr csD3
